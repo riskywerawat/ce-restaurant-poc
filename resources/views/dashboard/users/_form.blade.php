d0d5ad71e7a19ca18bcabbb215e0daeef047e495
[DEV] WEERAWAT.T <weerawat.t@pttdigital.com>
2023-02-03 00:56:01 +0700

    fix rms theme

--- resources/views/dashboard/users/_form.blade.php
+++ resources/views/dashboard/users/_form.blade.php
@@ -41,4 +41,79 @@
     </div>
 </div>
 
+<div class="mt-8 border-t border-gray-200 pt-8 sm:mt-5 sm:pt-10">
+    <div>
+        <h3 class="text-lg leading-6 font-medium text-gray-900">
+            Roles
+        </h3>
+        <p class="mt-1 max-w-2xl text-sm leading-5 text-gray-500">
+            {{ trans('dashboard/user.form.role_description') }}
+        </p>
+    </div>
+    <div class="mt-6 sm:mt-5">
+        <div class="sm:border-t sm:border-gray-200 sm:pt-5">
+            <div role="group" aria-labelledby="label-email">
+                <div class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-baseline">
+                    <div>
+                        <div class="text-base leading-6 font-medium text-gray-900 sm:text-sm sm:leading-5 sm:text-gray-700" id="label-email">
+                            Roles
+                        </div>
+                    </div>
+                    <div class="mt-4 sm:mt-0 sm:col-span-2">
+                        {{--@foreach($roles as $role)--}}
+                        {{--<div class="max-w-lg mt-4">--}}
+                            {{--<div class="relative flex items-start">--}}
+                                {{--<div class="flex items-center h-5">--}}
+                                    {{--<input id="role-{{ $role->name }}" type="radio" name="role" value="{{ $role->id }}"--}}
+                                            {{--@if($user->hasRole($role) || in_array($role->id, old('roles', []))) checked @endif--}}
+                                           {{--class="form-radio h-4 w-4 text-indigo-600 transition duration-150 ease-in-out" />--}}
+                                {{--</div>--}}
+                                {{--<div class="ml-3 text-sm leading-5">--}}
+                                    {{--<label for="role-{{ $role->name }}" class="font-medium text-gray-700 capitalize">{{ snakeCaseToText($role->name) }}</label>--}}
+                                    {{--<p class="text-gray-500">{{ $role->present()->description }}</p>--}}
+                                {{--</div>--}}
+                            {{--</div>--}}
+                        {{--</div>--}}
+                        {{--@endforeach--}}
 
+                        @if(auth()->user()->isSuperAdmin())
+                        <?php $superAdminRole = $roles->firstWhere('name', \App\Models\Role::SUPER_ADMIN); ?>
+                        <div class="max-w-lg mt-4">
+                            <div class="relative flex items-start">
+                                <div class="flex items-center h-5">
+                                    <input id="role-{{ $superAdminRole->name }}" type="radio" name="role" value="{{ $superAdminRole->id }}"
+                                        @if($user->hasRole($superAdminRole) || in_array($superAdminRole->id, old('roles', []))) checked @endif
+                                        class="form-radio h-4 w-4 text-indigo-600 transition duration-150 ease-in-out" />
+                                </div>
+                                <div class="ml-3 text-sm leading-5">
+                                    <label for="role-{{ $superAdminRole->name }}" class="font-medium text-gray-700 capitalize">{{ snakeCaseToText($superAdminRole->name) }}</label>
+                                    <p class="text-gray-500">{{ $superAdminRole->present()->description }}</p>
+                                </div>
+                            </div>
+                        </div>
+                        @endif
+
+                        <?php $adminRole = $roles->firstWhere('name', \App\Models\Role::ADMIN); ?>
+                        <div class="max-w-lg mt-4">
+                            <div class="relative flex items-start">
+                                <div class="flex items-center h-5">
+                                    <input id="role-{{ $adminRole->name }}" type="radio" name="role" value="{{ $adminRole->id }}"
+                                           @if($user->hasRole($adminRole) || in_array($adminRole->id, old('roles', []))) checked @endif
+                                           class="form-radio h-4 w-4 text-indigo-600 transition duration-150 ease-in-out" />
+                                </div>
+                                <div class="ml-3 text-sm leading-5">
+                                    <label for="role-{{ $adminRole->name }}" class="font-medium text-gray-700 capitalize">{{ snakeCaseToText($adminRole->name) }}</label>
+                                    <p class="text-gray-500">{{ $adminRole->present()->description }}</p>
+                                </div>
+                            </div>
+                        </div>
+
+
+
+                        <x-form-input-error for="role"/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
