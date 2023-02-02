5342d36260ff4c67c36df282efc73960d7f73c5c
[DEV] WEERAWAT.T <weerawat.t@pttdigital.com>
2023-02-03 01:05:54 +0700

    update seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,7 +18,7 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
         $this->call(RolesSeeder::class);
         $this->call(UsersSeeder::class);
-        $this->call(SiteSettingsSeeder::class);
+
         $this->call(KitchenSeeder::class);
         $this->call(MenuSeeder::class);
         $this->call(OrderRequestSeeder::class);
